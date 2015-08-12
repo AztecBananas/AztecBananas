a70db16c3f6ca9f39188b698e60f6befc0fba140
Kate Cobb <KateEcobb@gmail.com>
2015-08-12 16:28:59 -0700

    (feat)SQL helper function returning JSON, just need to integrate into middleware.

--- server/db/SQL/SQLController.js
+++ server/db/SQL/SQLController.js
@@ -1,28 +1,10 @@
-var mysql = require('mysql'); 
+var SQLdb = require('./SQLInteractor.js')
 
-var connection = mysql.createConnection({ 
-  user: 'root', 
-  password: '', 
-  database: 'PoliticalData'
-});
-
-connection.connect();
-
-//initializes the joinedData table with information from all three tables
-var init = function(){ 
-  var queryString = 'insert into joinedData select candidate.CAND_NAME, candidate.CAND_PTY_AFFILIATION, \
-  candidate.CAND_ELECTION_YR, candidate.CAND_OFFICE_ST, candidate.CAND_OFFICE_DISTRICT, committees.CMTE_NM, \
-  committees.CMTE_PTY_AFFILIATION, cont_to_cand.TRANSACTION_AMT from candidate inner join cont_to_cand \
-  on candidate.CAND_ID = cont_to_cand.CAND_ID inner join committees on cont_to_cand.CMTE_ID = committees.CMTE_ID;'
-  
-  connection.query(queryString, function(err, results){ 
-    if(err) console.log(err)
-  });
-
+var getFinancialData = function(req, res, next){ 
+  SQLdb.getContributions(function(err, results){ 
+    if(err) console.log("There was an issue with the GET request")
+    res.send(results)
+  })
 };
 
-
-
-
-
-
+exports.getFinancialData = getFinancialData;
